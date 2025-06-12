4ae4cedfefec1828bbd2c2a27e17f98ce61b0f63
stephanofer <slowdown_4@hotmail.com>
2025-06-11 21:08:50 -0500

    Align with zKoth API v3+

--- src/main/java/fr/maxlego08/zkoth/api/event/events/KothHookEvent.java
+++
@@ -1,8 +0,0 @@
-package fr.maxlego08.zkoth.api.event.events;
-
-import fr.maxlego08.zkoth.api.FactionListener;
-
-public class KothHookEvent {
-	public void setFactionListener(FactionListener factionListener) {
-	}
-}
